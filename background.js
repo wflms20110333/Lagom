bdf7f41c3449e2f6a239638f0bf92419ec7150ad
Nancy Shaw <33583144+itsnshaw@users.noreply.github.com>
2018-02-24 12:33:32 -0800

    Add files via upload

--- background.js
+++ background.js
@@ -1,29 +1,14 @@
-// Called when the user clicks on the browser action.
-/*
-chrome.browserAction.onClicked.addListener(function(tab) {
-  // Send a message to the active tab
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    var activeTab = tabs[0];
-    chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
-  });
-});
-*/
+console.log("Loaded extension");
 
-//location.href = 'http://www.usaco.org';
 
-/*
-var host = "http://tpb.pirateparty.org.uk";
-chrome.webRequest.onBeforeRequest.addListener(
-    function(details) {
-         return {redirectUrl: host + details.url.match(/^https?:\/\/[^\/]+([\S\s]*)/)[1]};
-    },
-    {
-        urls: [
-            "*://piratebay.se/*",
-            "*://www.piratebay.se/*"
-        ],
-        types: ["main_frame", "sub_frame", "stylesheet", "script", "image", "object", "xmlhttprequest", "other"]
-    },
-    ["blocking"]
-);
-*/
+function blockRequest(details) {
+   return {cancel: true};
+}
+
+function updateFilters(urls) {
+   if(chrome.webRequest.onBeforeRequest.hasListener(blockRequest))
+     chrome.webRequest.onBeforeRequest.removeListener(blockRequest);
+   chrome.webRequest.onBeforeRequest.addListener(blockRequest, {urls: ["*://*.facebook.com/*", "*://*.facebook.net/*"]}, ['blocking']);
+}
+
+updateFilters();
