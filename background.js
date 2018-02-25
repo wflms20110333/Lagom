d45f008cf2d563b1b2ba7d69a73cc062666cf6b5
wflms20110333 <elizabeth.zou.0@gmail.com>
2018-02-25 08:06:05 -0600

    Add files via upload

--- background.js
+++ background.js
@@ -1,153 +1,161 @@
-console.log("Loaded extension");
 var urls = [];   //    *://*.facebook.com/*
-//storage = window.localStorage;
- 
- function addURL(details){
+
+function addURL(details){
     urls.push(details);
- }
-
- function deleteURL(url){
- 	var index = urls.indexOf(url);
- 	//alert(index);
- 	if(index > -1)
- 		urls.splice(index, 1);
- }
-
- function setDefaultUrls(){
- 	return{
- 		urls:[
- 			'facebook.com',
- 			'twitch.tv',
- 			'reddit.com',
- 			'youtube.com',
- 			'netflix.com',
- 			'twitter.com',
- 			'tumblr.com'
- 		]
- 	}
- }
-
-//if it's already in the storage, 
- function loadUrls() {
-  if(localStorage.get('list') !== 'undefined'){
-  	return updatePrefsFormat(JSON.parse(localStorage.get('list')));
-  }
-  else{
-  	return savePrefs(urls);
-  }
-  // if(typeof localStorage['prefs'] !== 'undefined') {
-  //   return updatePrefsFormat(JSON.parse(localStorage['prefs']));
-  // } else {
-  //   return savePrefs(defaultUrls());
-  // }
 }
 
-function updatePrefsFormat(prefs){
-	/*	urls = prefs;
-		delete prefs;
-		savePrefs(urls);*/
-	return prefs;
-	// if(prefs.hasOwnProperty('blacklistinput')){
-	// 	prefs.urls = prefs.blacklistinput;
-	// 	delete prefs.blacklistinput;
-	// 	savePrefs(prefs);
-	// }
-	// return prefs;
+function deleteURL(url){
+    var index = urls.indexOf(url);
+    if(index > -1)
+        urls.splice(index, 1);
 }
 
-//save in to storage
-function savePrefs(prefs){
-	storage.setItem("list", JSON.stringify(urls));
-	return urls;
-	// localStorage['prefs'] = JSON.stringify(urls);
-	// return prefs;
+function format(url) {
+    if (url.startsWith("https://"))
+        url = url.substring(8);
+    if (url.startsWith("http://"))
+        url = url.substring(7);
+    if (url.startsWith("www."))
+        url = url.substring(4);
+    return url;
 }
 
- function blockRequest(details) {
-    return {cancel: true};
- }
- 
-function updateFilters(urls) {
-    if(chrome.webRequest.onBeforeRequest.hasListener(blockRequest))
-      chrome.webRequest.onBeforeRequest.removeListener(blockRequest);
-    chrome.webRequest.onBeforeRequest.addListener(blockRequest, {urls: urls}, ['blocking']);
- }
-
- function addSlash(url) {
-
- 	var arr = url.split("https://");
- 	var str = arr.join("");
- 	alert(str);
- 	return "*://*.".concat(str).concat("/*");
- }
-
- function ValidURL(str) {
- 	//alert("inside valid");
- 	var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
-    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
-    '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
-    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
-    '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
-    '(\\#[-a-z\\d_]*)?$','i'); // fragment locater
-  	if(!pattern.test(str)) {
-    	alert("Please enter a valid URL.");
-    	return false;
-  	} else {
-    	return true;
-  	}
+function ValidURL(str) {
+    var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
+        '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
+        '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+        '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+        '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+        '(\\#[-a-z\\d_]*)?$','i'); // fragment locater
+    if(!pattern.test(str)) {
+        alert("Please enter a valid URL.");
+        return false;
+    } 
+    else {
+        return true;
+    }
 }
 
-var timeLeft1 = 50000000000;
-var timeLeft2 = 300000000;
+
+
+var time1 = 1000000000;
+var time2 = 1000000000;
+var time3 = 1000000000;
+var time4 = 1000000000;
+
+var timesSet = false;
+
+var on = false;
+var blacklisting = false;
+var directing = false;
+
 chrome.runtime.onMessage.addListener(
-     function(request, sender, sendResponse) {
-//     	console.log("hi");
+    function(request, sender, sendResponse) {
+        if (request.greeting == "hello")
+            sendResponse({farewell: "goodbye", direct: directing});
+        else if (request.greeting == "herestheinput") {
+            time1 = request.input1 * 1000; // change this to minutes!!
+            time2 = request.input2 * 1000; // change this to minutes!!
+            time3 = request.input3 * 1000; // change this to minutes!!
+            time4 = request.input4 * 1000; // change this to minutes!!
 
-/*        if (request.greeting == "hello"){
-            sendResponse({farewell: "goodbye", time1: timeLeft1, time2: timeLeft2});
-            console.log("hi");
-        }*/
+            timesSet = true;
+            on = true;
+            blacklisting = true;
+            chrome.runtime.sendMessage({greeting: "focusOn"}, function(response) {});
 
-/*        else if (request.greeting == "herestheinput") {
-            timeLeft1 = request.input1 * 1000;
-            timeLeft2 = request.input2 * 1000;
-            console.log("hi");
-
-            //alert(timeLeft);
-        }*/
-        if (request.greeting == "blacklistinput") {
-        	if (ValidURL(request.input4)) {
-        		var parsed = addSlash(request.input4);
-	        	addURL(parsed);
-
-	        	//alert(urls.length);
-	        	updateFilters(urls);
-        	}
-	        	//urls.push("*://*.facebook.com/*");
-	    
-       		alert(urls);
-	        	//alert(urls.length);
-	        updateFilters(urls);
-        	//urls.push(request.input4);
+            setTimeout(notBlacklistMode, time1);
+            setTimeout(directMode, time3);
+            reloadAllTabs();
         }
-
-        if (request.greeting == "whitelistinput") {
-        	if (ValidURL(request.input3)) {
-        		//alert("here");
-        		var parsed = addSlash(request.input3);
-	        	deleteURL(parsed);
-
-	        	//alert(urls.length);
-	        	updateFilters(urls);
+        else if (request.greeting == "checkIfSet") {
+            sendResponse({set: timesSet});
+        }
+        else if (request.greeting == "turnOn") {
+            on = true;
+            blacklisting = true;
+            chrome.runtime.sendMessage({greeting: "focusOn"}, function(response) {});
+            setTimeout(notBlacklistMode, time1);
+            directing = false;
+            setTimeout(directMode, time3);
+        }
+        else if (request.greeting == "turnOff") {
+            on = false;
+            blacklisting = false;
+            chrome.runtime.sendMessage({greeting: "focusOff"}, function(response) {});
+            directing = false;
+        }
+        else if (request.greeting == "checkState") {
+            sendResponse({started: on, on1: blacklisting, on2: directing});
+        }
+        else if (request.greeting == "blacklistinput") {
+        	if (ValidURL(request.input4)) {
+                if (urls.indexOf(request.input4) < 0) {
+                    addURL(format(request.input4));
+                    reloadAllTabs();
+                }
         	}
-        	alert(urls);
-	        	//alert(urls.length);
-	        updateFilters(urls);
-        	//urls.push(request.input4);
-	    }
+        }
+        else if (request.greeting == "whitelistinput") { // deletes off black list
+        	if (ValidURL(request.input3)) {
+                var now = format(request.input3);
+                if (blacklisting && urls.indexOf(now) >= 0)
+                    alert("You can't remove a currently blacklisted site while in FOCUS mode!");
+	        	else {
+                    deleteURL(now);
+                    reloadAllTabs();
+                }
+        	}
+        }
+        else if (request.greeting == "checkIfBlocked") {
+            var seen = false;
+            for (x in urls) {
+                if (request.url.includes(urls[x])) {
+                    seen = true;
+                    break;
+                }
+            }
+            sendResponse({blocked: seen});
+        }
     }
 );
- 
-//addURL("*://*.facebook.com/*");
-//urls.push("*://*.facebook.com/*");
-updateFilters(urls);
+
+function blacklistMode() {
+    if (!on)
+        return;
+    blacklisting = true;
+    chrome.runtime.sendMessage({greeting: "focusOn"}, function(response) {});
+    setTimeout(notBlacklistMode, time1);
+}
+
+function notBlacklistMode() {
+    if (!on)
+        return;
+    blacklisting = false;
+    chrome.runtime.sendMessage({greeting: "focusOff"}, function(response) {});
+    setTimeout(blacklistMode, time2);
+}
+
+function directMode() {
+    if (!on)
+        return;
+    directing = true;
+    chrome.runtime.sendMessage({greeting: "lagomOn"}, function(response) {});
+    setTimeout(notDirectMode, time4);
+}
+
+function notDirectMode() {
+    if (!on)
+        return;
+    directing = false;
+    chrome.runtime.sendMessage({greeting: "lagomOff"}, function(response) {});
+    setTimeout(directMode, time3);
+}
+
+function reloadAllTabs() {
+    chrome.tabs.query({windowType:'normal'}, function(tabs) {
+        for(var i = 0; i < tabs.length; i++) {
+            chrome.tabs.update(tabs[i].id, {url: tabs[i].url});
+        }
+    });
+}
--- content.js
+++ content.js
@@ -1,13 +1,12 @@
+check();
+setInterval(check, 1000); // change this to one minute
 
-chrome.runtime.sendMessage({greeting: "hello"}, function(response) {
-  if (window.location.href == "http://www.usaco.org/")
-    window.onload = setTimeout(goBack, response.time2);
-  else if (!window.location.href.endsWith("options.html"))
-    window.onload = setTimeout(redirect, response.time1);
-});
+function redirect2() {
+  window.location.href = 'https://www.aboutlagom.com/focus';
+}
 
 function redirect() {
-  window.location.href = 'http://www.usaco.org/';
+  window.location.href = 'https://www.aboutlagom.com/resume';
 }
 
 function goBack() {
@@ -17,4 +16,25 @@ function goBack() {
     history.go(-1);
 }
 
-//chrome.tabs.reload();
+function check() {
+  chrome.runtime.sendMessage({greeting: "checkState"}, function(response) {
+    if (window.location.href == 'https://www.aboutlagom.com/resume') {
+      if (!response.on2)
+        goBack();
+    }
+    else if (!window.location.href.endsWith("options.html")) {
+      if (response.on2)
+        redirect();
+      else if (window.location.href == 'https://www.aboutlagom.com/focus') {
+        if (!response.on1)
+          goBack();
+      }
+      else if (response.on1) {
+        chrome.runtime.sendMessage({greeting: "checkIfBlocked", url: window.location.href}, function(response2) {
+          if (response2.blocked)
+            redirect2();
+        });
+      }
+    }
+  });
+}
